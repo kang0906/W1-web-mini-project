88b13c7da7edd4934419f2850c97605ae00ae9a9
kimminseok <kms3288381@naver.com>
2022-09-21 13:51:09 +0900

    로그아웃 기능 구현, 백그라운드 이미지 설정

--- game-is-my-life/static/myjs.js
+++ game-is-my-life/static/myjs.js
@@ -114,3 +114,8 @@ function post() {
         }
     });
 }
+ function sign_out() {
+                $.removeCookie('mytoken', {path: '/'});
+                alert('로그아웃!')
+                window.location.href = "/login"
+            }
